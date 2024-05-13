90f8c1f8647d61829fce67ebdc3a667da93ae61f
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-05-14 00:02:27 +0200

    chat colors , bubles layout

--- app.js
+++ app.js
@@ -36,15 +36,35 @@ setInterval(() => {
   io.emit('time', time) // Emitting the time as before
 }, 1000)
 
+// Object to store user colors
+const userColors = {}
+
+const predefinedColors = ['#07847e', '#7549a7', '#99ceb4', '#c3af58', '#e4a9ef']
+let colorIndex = 0
+
 io.on('connection', (socket) => {
+  // Generate a random color for the user
+  // const userColor = getRandomColor()
+
+  // Select a color from the predefined array
+  const userColor = predefinedColors[colorIndex]
+
+  // Store the user color based on their socket ID
+  userColors[socket.id] = userColor
+
   console.log(`A client with id ${socket.id} connected to the chat!`)
 
+  // Increment the color index for the next user
+  colorIndex = (colorIndex + 1) % predefinedColors.length
+
   socket.on('chatMessage', (msg) => {
-    console.log('Message: ' + msg.user + ' ' + msg.message + msg.time)
+    console.log('Message: ' + msg.user + ' ' + msg.message)
+
     io.emit('newChatMessage', {
       user: msg.user,
       message: msg.message,
       date: new Date(),
+      color: userColors[socket.id], // Send the user color with the message
     })
     let today = new Date()
     let dateTime = today.toLocaleString()
@@ -60,8 +80,20 @@ io.on('connection', (socket) => {
   })
   socket.on('disconnect', () => {
     console.log(`Client ${socket.id} disconnected!`)
+    // Remove the user color when the user disconnects
+    delete userColors[socket.id]
   })
 })
+
+// Function to generate a random color
+// function getRandomColor() {
+//   const letters = '0123456789ABCDEF'
+//   let color = '#'
+//   for (let i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * 16)]
+//   }
+//   return color
+// }
 
 server.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`)
--- public/client.js
+++ public/client.js
@@ -13,7 +13,42 @@ let myUser
 formUser.addEventListener('submit', function (e) {
   e.preventDefault()
   myUser = inputUser.value
-  userContainer.innerHTML = '<h2>Hi ' + myUser + '</h2>'
+
+  //photo selctection based on the gender
+  const gender = document.getElementById('genderSelect').value
+  // generate a random number between 1 and 50 to change the photo is provide by the api
+  const randomNumber = Math.floor(Math.random() * 50) + 1
+
+  // userContainer.innerHTML = '<h2>Hi ' + myUser + '</h2>'
+  // Create elements for user information
+  const topDiv = createNode('div')
+  topDiv.classList.add('top')
+
+  const userDiv = createNode('div')
+  userDiv.classList.add('user-info')
+
+  const img = createNode('img')
+  img.src = `https://randomuser.me/api/portraits/${gender}/${randomNumber}.jpg`
+  img.alt = `${myUser}`
+
+  const userStatusDiv = createNode('div')
+  userStatusDiv.id = 'user-status'
+
+  const strong = createNode('strong')
+  strong.textContent = myUser
+
+  const statusDiv = createNode('div')
+  statusDiv.classList.add('status')
+  statusDiv.textContent = 'Online'
+
+  // Append elements to user container
+  append(userDiv, img)
+  append(topDiv, userDiv)
+  append(userStatusDiv, strong)
+  append(userStatusDiv, statusDiv)
+  append(topDiv, userStatusDiv)
+  append(userContainer, topDiv)
+
   document.querySelector('#user').style.display = 'none'
   document.querySelector('#chat-message').style.display = 'block'
 })
@@ -38,7 +73,29 @@ socket.on('time', (timeMsg) => {
 socket.on('newChatMessage', function (msg) {
   let item = document.createElement('li')
   const formattedDate = new Date(msg.date).toLocaleString()
-  // item.textContent = msg
-  item.textContent = msg.user + ': ' + msg.message + ' (' + formattedDate + ')'
+
+  item.classList.add('message')
+
+  if (msg.user === myUser) {
+    item.classList.add('you')
+  }
+
+  // Add a unique class to the message element based on the user who posted it
+  item.classList.add('message-' + msg.user.toLowerCase())
+
+  // Apply a random background color to the message
+  item.style.backgroundColor = msg.color
+
+  // item.textContent = msg.user + ': ' + msg.message + ' (' + formattedDate + ')'
+  item.textContent = msg.user + ': ' + msg.message
+
+  // Crear un elemento span para la fecha y aplicar la clase "date"
+  let dateSpan = document.createElement('span')
+  dateSpan.textContent = formattedDate
+  dateSpan.classList.add('date')
+
+  // Agregar el span de fecha al elemento li
+  item.appendChild(dateSpan)
+
   messages.appendChild(item)
 })
